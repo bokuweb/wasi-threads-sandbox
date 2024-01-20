a1f7ffeb0400ef90b7c68aac6a370a654678195b
bokuweb <bokuweb12@gmail.com>
2024-01-20 18:55:15 +0900

    rayon

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const { WASI } = require("wasi");
 const { argv, env } = require("node:process");
 const { join } = require("node:path");
 
-const wasi = new WASI({ version: "preview1", args: argv, env });
-
 const file = readFile(join(__dirname, "wasi-threads.wasm"));
+
+const wasi = new WASI({ version: "preview1", args: argv, env });
 
 if (isMainThread) {
   (async () => {
@@ -31,23 +31,33 @@ if (isMainThread) {
     wasi.start(instance);
   })();
 } else {
-  parentPort.on("message", async (start_arg) => {
-    const wasm = await WebAssembly.compile(await file);
-    const { memory } = workerData;
-    const instance = await WebAssembly.instantiate(wasm, {
-      ...wasi.getImportObject(),
-      wasi: {
-        "thread-spawn": (arg) => {
-          const worker = new Worker(__filename, { workerData: { memory } });
-          worker.postMessage(arg);
+  const handler = async (start_arg) => {
+    try {
+      const wasm = await WebAssembly.compile(await file);
+      const { memory } = workerData;
+      const instance = await WebAssembly.instantiate(wasm, {
+        ...wasi.getImportObject(),
+        wasi: {
+          "thread-spawn": (arg) => {
+            const worker = new Worker(__filename, { workerData: { memory } });
+            worker.postMessage(arg);
+          },
         },
-      },
-      env: { memory },
-    });
-    // thread id and start_arg
-    instance.exports.wasi_thread_start(1, start_arg);
-    process.exit(0);
-  });
+        env: { memory },
+      });
+      // thread id and start_arg
+      instance.exports.wasi_thread_start(1, start_arg);
+      parentPort.removeListener("message", handler);
+    } catch (e) {
+      if (e.code.includes("ERR_WASI_NOT_STARTED")) {
+        // NOP
+        return;
+      }
+      throw e;
+    } finally {
+      process.exit(0);
+    }
+  };
+  parentPort.addListener("message", handler);
 }
 
-//
